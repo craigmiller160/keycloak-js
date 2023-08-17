16f4cd614fc81fe16103f7d6b4be703342064ebe
Craig Miller <craigmiller160@gmail.com>
2023-08-17 12:53:44 -0400

    Added libs

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -2,7 +2,6 @@ export default {
 	extends: [
 		'@craigmiller160/eslint-config-js',
 		'@craigmiller160/eslint-config-prettier',
-		'@craigmiller160/eslint-config-react',
 		'@craigmiller160/eslint-config-ts'
 	]
 };
